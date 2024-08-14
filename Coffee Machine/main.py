243e1654dae8400d880f5135c70f2f8c63f6369c
Andrew Baker <bakerandrew4@gmail.com>
2024-08-14 12:42:18 -0600

    update

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -3,7 +3,12 @@ import menu
 menu_list = menu.MENU
 resources_list = menu.resources
 
+request = input("What drink would you like? Espresso, latte, or cappuccino?\n")
 
-
-
-
+def check_resources(resource_list, menu, menu_item):
+    item = menu[menu_item]
+    ingredients = item["ingredients"]
+    for n in ingredients:
+        print(ingredients[n])
+    
+check_resources(resources_list, menu_list, request)
